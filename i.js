3c22e3cd2499056a704a480ba2ab7ccf6fbacbe1
DevMBS <78100892+DevMBS@users.noreply.github.com>
2021-03-21 17:58:17 +0300

    Update i.js

--- i.js
+++ i.js
@@ -1,9 +1,8 @@
-pFAR = "";
+//checking password
 function checkUser(){
   $.ajax({
     url:'uploads/peofjpienrofiu34785y389470r9123-8jfun4un0u8h349fh098hq0uj0iweufn0ui0F7Q04UFJ0EJ-F9H0.sspfmbsc',
     success: function (data){
-      pFAR = data;
       if(document.getElementById("password").value == window.atob(data)){
         document.getElementById("ok").innerHTML = '<h1 id="uf"><p align="center">Upload file</p></h1><br/><form action="/upload" method="post" enctype="multipart/form-data"><label>File:</label><br/><br/><input type="file" name="filedata" id="file"/><br><br><input class="s" type="submit" value="SEND" /></form><button class="up" onclick="location.href=\'/uploads\';">UPLOADS</button>';
         document.getElementById("passinput").style.display="none";
