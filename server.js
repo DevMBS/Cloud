2651acf558c2b0d02b47c26d33a9826d5481dc69
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2021-03-26 19:36:22 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-//Initalizing node modules
 const express = require("express");
 const multer  = require("multer");
 const app = express();
 const fs = require('fs')
-
-//configuring multer
 const storageConfig = multer.diskStorage({
     destination: (req, file, cb) =>{
         cb(null, "uploads/.uploads");
@@ -15,22 +12,30 @@ const storageConfig = multer.diskStorage({
 });
  
 app.use(express.static(__dirname));
-//upload file
+
 app.use(multer({storage:storageConfig}).single("filedata"));
 app.post("/upload", function (req, res, next) {
-    //writing information about the name of the uploaded file to the files.filedata
+   
     let filedata = req.file;
     fs.appendFile('uploads/files.filedata', String(filedata.filename)+" ", (err) => {
     if (err) {
       console.error(err)
       return
     }
-    })
-    //displaying information that the file has been uploaded (or not)
+    });
     if(!filedata)
         res.send("Failed to upload file<script>location.href='..//uploads';</script>");
     else
         res.send("File uploaded<script>location.href='..//uploads';</script>");
 });
-//starting express server
+app.post('/delete', function(req, res) {
+    var ffd = req.body.delete;
+    fs.unlink("uploads/.uploads/"+ffd, (err) => {
+      if(err){
+      }
+      else{
+        res.send("<script>location.href='..//uploads';</script>");
+      }
+    });
+ });
 app.listen(3000, ()=>{console.log("Server started");});
