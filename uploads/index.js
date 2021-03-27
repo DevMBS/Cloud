e3a1137cf4b761b794784fb96d6a15431414cb53
Mikhail Kuznetsov <78100892+DevMBS@users.noreply.github.com>
2021-03-27 14:34:19 +0300

    Update index.js

--- uploads/index.js
+++ uploads/index.js
@@ -19,9 +19,47 @@ function getFiles(){
 }
 function innerMenu(list){
 
-  document.getElementById('span'+list).innerHTML = "<a href='.uploads/"+list+"' download class='a'>DOWNLOAD FILE</a><form action='/delete' method='post' enctype='multipart/form-data'><input type='text' name='delete' id='file' value='"+list+"' style='display:none;'/><input type='submit' value='DELETE FILE' class='df'/></form><div class='cl-btn-7' onclick='document.getElementById(`span"+list+"`).innerHTML = ``;'></div>";
+  document.getElementById('span'+list).innerHTML = "<a href='.uploads/"+list+"' download class='a'>DOWNLOAD FILE</a><button class='openFile' onclick='openfile(`"+list+"`);'>OPEN FILE</button><form action='/delete' method='post' enctype='multipart/form-data'><input type='text' name='delete' id='file' value='"+list+"' style='display:none;'/><input type='submit' value='DELETE FILE' class='df'/></form><div class='cl-btn-7' onclick='document.getElementById(`span"+list+"`).innerHTML = ``;document.getElementById(`open`).innerHTML = ``;'></div>";
 
 
+}
+function openfile(file){
+  const extension = file.split(".")[1];
+  switch(extension){
+    case "txt":
+      $.ajax({
+        url:'.uploads/'+file,
+        success: function (data){
+          document.getElementById("open").innerHTML = "<div class='txt'>"+data+"</div>";
+        }
+      });
+      break;
+    case "png":
+      document.getElementById("open").innerHTML = "<img src='.uploads/"+file+"'/>";
+      break;
+    case "jpg":
+      document.getElementById("open").innerHTML = "<img src='.uploads/"+file+"'/>";
+      break;
+    case "jpeg":
+      document.getElementById("open").innerHTML = "<img src='.uploads/"+file+"'/>";
+      break;
+    case "svg":
+      document.getElementById("open").innerHTML = "<img src='.uploads/"+file+"'/>";
+      break;
+    case "mp4":
+      document.getElementById("open").innerHTML = `<video controls="controls"><source src=".uploads/`+file+`" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'></video>`;
+      break;
+    case "ogv":
+      document.getElementById("open").innerHTML = `<video controls="controls"><source src=".uploads/`+file+`" type='video/ogg; codecs="theora, vorbis"'></video>`;
+      break;
+    case "webm":
+      document.getElementById("open").innerHTML = `<video controls="controls"><source src=".uploads/`+file+`" type='video/webm; codecs="vp8, vorbis"'></video>`;
+      break;
+    default:
+      document.getElementById("open").innerHTML = "This file type is not supported";
+      break;
+    
+  }
 }
 function checkUser(){
   $.ajax({
@@ -29,7 +67,7 @@ function checkUser(){
     success: function (data){
       pFAR = data;
       if(document.getElementById("password").value == window.atob(data)){
-        document.getElementById("uploads").innerHTML = '<button  type="button" id="btnGetTxt" onclick="getFiles()">See list of files</button><div id="files"></div>';
+        document.getElementById("uploads").innerHTML = '<button  type="button" id="btnGetTxt" onclick="getFiles()">See list of files</button><div id="open"></div><div id="files"></div>';
         document.getElementById("passinput").style.display="none";
       }
       else{
